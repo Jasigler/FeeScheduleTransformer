71511a1f6c5a1f10bc7e1ca0a9ec3443ea254f6e
Jason Sigler <jsigler2@asu.edu>
2019-03-04 09:04:43 -0500

    Menu & Refactor (#2)
    * Refactoring into reusable functions
    
    * Code cleanup
    
    * Added refactored .py
    
    * Merged into main file

--- FeeScheduleTransformer.py
+++ FeeScheduleTransformer.py
@@ -7,44 +7,77 @@
 #  
 #####################
 
-from os import listdir
-from os.path import *
-import xlwt 
-import xlutils
+import os
+import xlwt
+import PyPDF2
+import textract
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
-#Initialize the workbook and define the output target
-data_destination = xlwt.Workbook()
-sheet = data_destination.add_sheet('feeSchedule')  
 
-#Initialize variables
-row = 0
-modifierRequired = '00'
-lCode = ''
-allowable = ''
-codeModifier = ''
 
-#open the source file
-file = open('MedicaidInput.txt')
 
-#Iterates through the input, separates each line and transforms it into a list, then writes
-#only codes with the specified modifier to the file. 
-#
-for line in file:                     
-	line = line.strip()
-	if line:
-		splitLine = line.split(" ")
-		lCode = splitLine[0]
-		codeModifier = splitLine[1]
-		allowable = splitLine[-1]
-		if codeModifier == modifierRequired:
-			sheet.write(row,0,lCode)
-			sheet.write(row,1,allowable)
-			row += 1
-		
-		
-			
+def ConvertToExcel(inputFile, outputFile, modifier):
+    row = 0
+    modifierRequired = modifier
+    try:
+        file = open(inputFile)
+    except FileExistsError:
+        print("Invalid filename")
+        UserMenu()
+    except FileNotFoundError:
+        print("File Not Found")
+        UserMenu()
+    except:
+        print("Something went horribly wrong")
+        UserMenu()
 
-	
-			
-#Save the destination target
-data_destination.save('formattedschedule.xls')
+    data_destination = xlwt.Workbook()
+    sheet = data_destination.add_sheet('feeSchedule')
+
+    for line in file:
+        line = line.strip()
+        if line:
+            splitLine = line.split(" ")
+            lCode = splitLine[0]
+            codeModifier = splitLine[1]
+            allowable = splitLine[-1]
+            if codeModifier == modifierRequired:
+                sheet.write(row, 0, lCode)
+                sheet.write(row, 1, allowable)
+                row += 1
+    data_destination.save(outputFile)
+    print(str(row) + " Rows Processed.")
+    UserMenu()
+
+
+def UserMenu():
+    print("Fee Schedule Transformer")
+    print("................")
+    print("1. Scrape PDF")
+    print("2. Convert TXT to XLS")
+    print("3. Quit\n")
+    userChoice = input("Make a Selection:")
+
+    if userChoice == '1':
+        print("Not Available")
+        UserMenu()
+    elif userChoice == '2':
+        sourceFile = input("Enter the source filename:")
+        destinationFile = input("Enter the destination filename:")
+        requiredModifier = input("Enter the modifier to search against:")
+        ConvertToExcel(sourceFile, destinationFile, requiredModifier)
+    elif userChoice == '3':
+        exit()
+    else:
+        print("Not a valid entry")
+        UserMenu()
+
+
+
+
+
+
+
+
+UserMenu()
