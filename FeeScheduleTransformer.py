94219313360ac6a310f8a2d1d11c7f19daf6070f
Jason Sigler <jason.sigler@outlook.com>
2019-03-04 14:15:12 -0500

    Code Clean-up

--- FeeScheduleTransformer.py
+++ FeeScheduleTransformer.py
@@ -6,34 +6,29 @@
 #
 #  
 #####################
-
-import os
 import xlwt
-import PyPDF2
-import textract
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-
-
 
 
 def ConvertToExcel(inputFile, outputFile, modifier):
     row = 0
     modifierRequired = modifier
+    #Try to open the user-defined file any catch any errors that occur
     try:
         file = open(inputFile)
     except FileExistsError:
-        print("Invalid filename")
+        print("Invalid filename\n")
         UserMenu()
     except FileNotFoundError:
-        print("File Not Found")
+        print("File Not Found\n")
         UserMenu()
     except:
-        print("Something went horribly wrong")
+        print("Something went horribly wrong\n")
         UserMenu()
 
+    #Initialize the new Excel document
     data_destination = xlwt.Workbook()
     sheet = data_destination.add_sheet('feeSchedule')
+
 
     for line in file:
         line = line.strip()
@@ -47,37 +42,26 @@ def ConvertToExcel(inputFile, outputFile, modifier):
                 sheet.write(row, 1, allowable)
                 row += 1
     data_destination.save(outputFile)
-    print(str(row) + " Rows Processed.")
+    print(str(row) + " Rows Processed.\n")
     UserMenu()
-
 
 def UserMenu():
     print("Fee Schedule Transformer")
     print("................")
-    print("1. Scrape PDF")
-    print("2. Convert TXT to XLS")
-    print("3. Quit\n")
+    print("1. Convert TXT to XLS")
+    print("2. Quit\n")
     userChoice = input("Make a Selection:")
 
     if userChoice == '1':
-        print("Not Available")
-        UserMenu()
-    elif userChoice == '2':
         sourceFile = input("Enter the source filename:")
         destinationFile = input("Enter the destination filename:")
         requiredModifier = input("Enter the modifier to search against:")
         ConvertToExcel(sourceFile, destinationFile, requiredModifier)
-    elif userChoice == '3':
-        exit()
+    elif userChoice == '2':
+        exit(0)
     else:
         print("Not a valid entry")
         UserMenu()
-
-
-
-
-
-
 
 
 UserMenu()
